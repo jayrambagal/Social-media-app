4c14462896fb0d56234a2100e88853f13a92f6b8
Jayram Bagal <94613732+jayrambagal@users.noreply.github.com>
2023-04-16 17:55:21 +0530

    frontend is ready for deploy

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -66,7 +66,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
     
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://socialmedia-tuji.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -86,7 +86,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+    const loggedInResponse = await fetch("https://socialmedia-tuji.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
